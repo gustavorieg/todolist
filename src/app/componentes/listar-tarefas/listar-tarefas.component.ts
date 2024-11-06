4dd0b007278fd482f4f2418a6eb7fb56ce8b5920
Gustavo Rieg <gustavorieg10@gmail.com>
2024-11-06 12:22:54 -0300

    listar tarefas

--- src/app/componentes/listar-tarefas/listar-tarefas.component.ts
+++ src/app/componentes/listar-tarefas/listar-tarefas.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { Tarefa } from '../tarefa/tarefa.component';
+import { Tarefa, TarefaComponent } from '../tarefa/tarefa.component';
 
 @Component({
   selector: 'app-listar-tarefas',
   standalone: true,
-  imports: [],
+  imports: [
+    TarefaComponent
+  ],
   templateUrl: './listar-tarefas.component.html',
   styleUrl: './listar-tarefas.component.css'
 })
 export class ListarTarefasComponent {
-  listaTarefas: Tarefa[] = []
+  listaTarefas: Tarefa[] =
+  [
+    {
+      id: 1,
+      descricao: 'Desenvolver site',
+      situacao: 'ativo'
+    },
+    {
+      id: 2,
+      descricao: 'Desenvolver sistema',
+      situacao: 'finalizado'
+    }
+  ]
 }
